e7e7e9b3c06a8d1344899188aed296199345590a
v5star <31580191+v5star@users.noreply.github.com>
2026-02-19 16:31:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,6 +5,3 @@ if [[ $CONFIG_FILE == lean* ]]; then
 elif [[ $CONFIG_FILE == hanwckf* ]]; then
     sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
 fi
-
-# luci-theme-kucat 一个清爽的luci主题
-git clone -b v2.5-19.07 --depth=1 https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
