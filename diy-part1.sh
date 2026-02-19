76af4b28a1e7ff7a950b3b471760f83e793d83bd
v5star <31580191+v5star@users.noreply.github.com>
2026-02-19 16:40:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,3 +5,5 @@ if [[ $CONFIG_FILE == lean* ]]; then
 elif [[ $CONFIG_FILE == hanwckf* ]]; then
     sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
 fi
+# 3-添加 opentomcat 主题
+git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
