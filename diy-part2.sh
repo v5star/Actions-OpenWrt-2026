dfe920d7f1eb785b42196491780304cfaffddace
v5star <31580191+v5star@users.noreply.github.com>
2026-02-19 14:16:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,8 +6,8 @@ sed -i 's/192.168...1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 取消原主题luci-theme-bootstrap 为默认主题
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
-# 修改 luci-theme-KuCat 为默认主题
-#sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' feeds/luci/collections/luci/Makefile
+# 修改 argon kucat  为默认主题
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 删除原默认主题
 #rm -rf package/lean/luci-theme-bootstrap
